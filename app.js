c522f7e7b33f83752913ea7b945b5462cdb51416
Juan Camilo Alvarez <juancamiloalvarezsarmiento22@gmail.com>
2026-03-27 20:24:33 -0500

    feat: login con validacion de correo institucional closes #7

---
+++ app.js
@@ -0,0 +1,24 @@
+let intentos = 0;
+
+function login() {
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  const error = document.getElementById("error");
+
+  if (!email.endsWith("@sanjose.edu") || pass.length < 4) {
+    intentos++;
+
+    if (intentos >= 3) {
+      error.innerText = "Cuenta bloqueada temporalmente";
+      return;
+    }
+
+    error.innerText = "Usuario o contraseña incorrectos";
+    return;
+  }
+
+  // Guardar sesión
+  localStorage.setItem("user", email);
+
+  window.location.href = "dashboard.html";
+}
